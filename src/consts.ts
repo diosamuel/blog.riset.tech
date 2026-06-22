50289ea27848066f0495e1285b171da52afe41ad
diosamuel <koding12sam@gmail.com>
2026-06-22 09:15:28 +0700

    update: title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "blog.riset.tech by Virdio Samuel",
+  NAME: "blog by Virdio Samuel",
   EMAIL: "mail",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
